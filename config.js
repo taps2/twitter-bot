3bcda53df1363f2592498b27ce75306f94b89168
Josh <joshchilds2004@yahoo.com>
2017-08-31 20:01:23 -0500

    twitterbot
    node.js twitter bot that automatically responds based on certain
    criteria with a countdown of the time the President has left in office
    if he won the next election.

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'hWZoZV3KWMv00YxUbb7Vq4chL',
+  consumer_secret:      'c2Rv0NAKzBqxgieAMlAITsmRoDOCnymr0DROyJEEyOPzTs5dpV',
+  access_token:         '490841636-8fFeTeeToPKXgzUtCo51ePXPz4qNKRE9SuDEJi75',
+  access_token_secret:  'QQ1LSxkQ0sGIOHuf0oLIiiwfZ0tULum0wg7lQ2x4ylsLa'
+}
+
+module.exports = config;
